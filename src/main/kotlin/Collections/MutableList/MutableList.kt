eac031e7d4bf2a6a2d53efeb6d73aec173383e4f
Nkot117 <kumakura117@gmail.com>
2024-01-20 16:10:13 +0900

    MutableListのメモを追加

--- src/main/kotlin/Collections/MutableList/MutableList.kt
+++ src/main/kotlin/Collections/MutableList/MutableList.kt
@@ -1,17 +1,57 @@
 package org.example.Collections.MutableList
 
-fun main() {
-    val mutableList = mutableListOf(1,2,3,4,5,6,7,8,9)
+private val mutableList = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
+fun main() {
     // add
     mutableList.add(10)
-    println("add=${mutableList}")
+    check("add")
 
     // addAll
-    mutableList.addAll(listOf(11,12,13))
-    println("addAll=${mutableList}")
+    mutableList.addAll(listOf(11, 12, 13))
+    check("addAll")
 
     // clear
     mutableList.clear()
-    println("clear=${mutableList}")
+    check("clear")
+    init()
+
+    // remove
+    mutableList.remove(1)
+    check("remove")
+    init()
+
+    // removeAll
+    mutableList.removeAll(listOf(1, 2, 3))
+    check("removeAll")
+    init()
+
+    // removeAt
+    mutableList.removeAt(2)
+    check("removeAt")
+    init()
+
+    // retainAll
+    mutableList.retainAll(listOf(1,3,5))
+    check("retainAll")
+    init()
+
+    // set
+    mutableList.set(1,40)
+    check("set")
+    init()
+
+    // replaceAll
+    mutableList.replaceAll{it * 10}
+    check("replaceAll")
+    init()
+}
+
+fun init() {
+    mutableList.clear()
+    mutableList.addAll(mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9))
+}
+
+fun check(str: String){
+    println("${str}=${mutableList}")
 }
--- src/main/kotlin/Collections/MutableList/memo.md
+++ src/main/kotlin/Collections/MutableList/memo.md
@@ -11,3 +11,15 @@ https://runebook.dev/ja/docs/kotlin/api/latest/jvm/stdlib/kotlin.collections/-mu
   - 指定されたリストを追加する。
 - clear
   - 全ての要素を削除する。
+- remove
+  - 指定した要素をリストから削除する
+- removeAll
+  - 指定された要素を全て削除する 
+- removeAt
+  - 指定されたインデックスの要素を削除する
+- retainAll
+  - 指定されたリストのみを保持し、それ以外を削除する
+- set
+  - 指定されたインデックスの要素を指定された値に変更する
+- replaceAll
+  - リストの要素を変換する
